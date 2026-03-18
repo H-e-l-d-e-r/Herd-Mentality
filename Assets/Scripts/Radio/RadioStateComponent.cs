8e1d8d77747b97b2bb6784bc600453a9fdd91bc4
Earnest Blade <luca@marpeau.web4me.fr>
2026-03-18 14:35:06 +0100

    radio components

---
+++ Assets/Scripts/Radio/RadioStateComponent.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadioStateComponent : RadioComponentBehaviour<int>
+{
+    [Header("Parameters")]
+    [Range(1, 5)]
+    public int MaxState = 1;
+
+    [Header("Components")]
+    public Button Switch;
+
+    void Start()
+    {
+        NullComponents.ThrowIfNull(Switch);
+
+        // register callbacks
+        Switch.onClick.AddListener(UpdateBehaviour);
+
+        UpdateBehaviour();
+    }
+
+    void UpdateBehaviour()
+    {
+        m_value = (m_value + 1) % MaxState;
+    }
+}
