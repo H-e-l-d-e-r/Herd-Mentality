2546b50e88513bed45c2246d294606d0157b10a4
H-e-l-d-e-r <susukigeneration@gmail.com>
2026-04-19 23:41:21 +0200

    xxx

--- Assets/Scripts/Entities/EntitiesManager.cs
+++ Assets/Scripts/Entities/EntitiesManager.cs
@@ -7,28 +7,28 @@ using UnityEngine;
 public class EntitiesManager : MonoBehaviour
 {
     public GameObject EntityTemplate;
-    
+
     public EntitiesTablesPerDay[] Tables;
     public DialogueTable[] Barks;
 
     public Transform[] SpawnPoints;
 
-    private List<EntitesBehaviour> m_entities;
+    private List<EntityBehaviour> m_entities;
 
     private void Start()
     {
-        m_entities = new List<EntitesBehaviour>();
+        m_entities = new List<EntityBehaviour>();
         SpawnEntities(GameManager.Instance.CurrentDay);
     }
 
     void OnDisable()
     {
-        ClearEntities();  
+        ClearEntities();
     }
 
     void SpawnEntities(int day)
     {
-        if(day >= Tables.Length)
+        if (day >= Tables.Length)
         {
             Debug.Log($"no tables registered for day {day}!");
             return;
@@ -41,15 +41,15 @@ public class EntitiesManager : MonoBehaviour
         for (int i = 0; i < count; i++)
         {
             Transform target = SpawnPoints[UnityEngine.Random.Range(0, SpawnPoints.Length)];
-            EntitesBehaviour entity = Instantiate(EntityTemplate, target).GetComponent<EntitesBehaviour>();
-            
-            // check for correct type
-            Debug.Assert(entity.GetType() == typeof(EntitiesBot));
+            EntityBehaviour entity = Instantiate(EntityTemplate, target).GetComponent<EntityBehaviour>();
 
-            EntitiesBot bot = entity as EntitiesBot;
+            // check for correct type
+            Debug.Assert(entity.GetType() == typeof(EntityBot));
+
+            EntityBot bot = entity as EntityBot;
             bot.transform.localPosition = Vector3.zero;
             bot.PlayOnce = Tables[day].PlayOnce;
-            
+
             bot.Tables = new DialogueTable[Tables[day].Tables.Length];
             Array.Copy(Tables[day].Tables, bot.Tables, bot.Tables.Length);
 
@@ -59,7 +59,7 @@ public class EntitiesManager : MonoBehaviour
 
     void ClearEntities()
     {
-        foreach (EntitesBehaviour entity in m_entities)
+        foreach (EntityBehaviour entity in m_entities)
         {
             Destroy(entity.gameObject);
         }
