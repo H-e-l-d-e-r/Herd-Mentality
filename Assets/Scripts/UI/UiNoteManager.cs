a0594dfb15ed5b36fec3e10eb01583d4d11d1044
ampheino <38631146+ampheino@users.noreply.github.com>
2026-04-21 17:11:28 +0200

    debut code note

--- Assets/Scripts/UI/UiNoteManager.cs
+++ Assets/Scripts/UI/UiNoteManager.cs
@@ -2,22 +2,25 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiNoteManager : MonoBehaviour
 {
     public GameObject ZoneDisplay;
-    public TMP_Text textDisplay;
+    public GameObject[] NoteDontDisplay;
+    public TMP_Text[] NoteDisplay;
+    public Button Button;
 
     private List<CollectibleObject> m_spawnText;
     private int m_page = 0;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_spawnText = new();
 
-        CreateText(); 
-
+        CreateText();
     }
 
     // Update is called once per frame
@@ -26,20 +29,44 @@ public class UiNoteManager : MonoBehaviour
         
     }
 
+    // creer le texte a afichier depuis l'instance 
     void CreateText()
     {
         int index = (int)Mathf.Clamp(m_page, 0, GameManager.Instance.UnlockedCollectibles.Length - 1);
         CollectibleObject collectible = GameManager.Instance.UnlockedCollectibles[index];
-    }
+        Button.onClick.AddListener(() => { OnButtonClick(collectible); });
+        foreach (TMP_Text page in NoteDisplay)
+        {
+            page.text = collectible.Description;
+        }
 
+        m_spawnText.Add(collectible);   
+        // bz ton pere la tchoing page a la con de carnet 
+    }
+    // pour afficher prochaine page 
     public void NextPage()
     {
         m_page++;
     }
+    // pour afficher Previus page
     public void PreviusPage()
     {
         m_page--;
     }
+    // logique de click sur le bouton 
+    public void OnButtonClick(CollectibleObject collectible)
+    {
+        foreach (TMP_Text show in NoteDisplay)
+        {
+            show.gameObject.SetActive(true);
+        }
 
+
+        foreach (GameObject hide in NoteDontDisplay)
+        {
+            hide.gameObject.SetActive(false);
+        }
+
+    }
 
 }
