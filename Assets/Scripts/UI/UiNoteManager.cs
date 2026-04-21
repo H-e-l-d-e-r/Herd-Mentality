db2976b4fd710c2398d1e3fb52561413550f35e9
ampheino <Marc.colas.2005@gmail.com>
2026-04-21 22:22:14 +0200

    code ui note bon sauf pour code

--- Assets/Scripts/UI/UiNoteManager.cs
+++ Assets/Scripts/UI/UiNoteManager.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 
 public class UiNoteManager : MonoBehaviour
 {
-    public GameObject ZoneDisplay;
+    //public GameObject ZoneDisplay;
     public GameObject[] NoteDontDisplay;
     public TMP_Text[] NoteDisplay;
-    public Button Button;
+    public Button BtnOuvrirCarnet;
+    public Button BtnNext;
+    public Button BtnPrevious;
 
     private List<CollectibleObject> m_spawnText;
     private int m_page = 0;
@@ -21,6 +23,13 @@ public class UiNoteManager : MonoBehaviour
         m_spawnText = new();
 
         CreateText();
+
+        //BtnNext.onClick.AddListener(NextPage);
+        //BtnPrevious.onClick.AddListener(PreviusPage);
+        BtnOuvrirCarnet.onClick.AddListener(CreateText);
+
+        MettreAJourBoutons();
+        
     }
 
     // Update is called once per frame
@@ -32,29 +41,79 @@ public class UiNoteManager : MonoBehaviour
     // creer le texte a afichier depuis l'instance 
     void CreateText()
     {
-        int index = (int)Mathf.Clamp(m_page, 0, GameManager.Instance.UnlockedCollectibles.Length - 1);
-        CollectibleObject collectible = GameManager.Instance.UnlockedCollectibles[index];
-        Button.onClick.AddListener(() => { OnButtonClick(collectible); });
-        foreach (TMP_Text page in NoteDisplay)
+        //int index = (int)Mathf.Clamp(m_page, 0, GameManager.Instance.UnlockedCollectibles.Length - 1);
+        CollectibleObject[] collectible = GameManager.Instance.UnlockedCollectibles;
+        BtnOuvrirCarnet.onClick.AddListener(() => { OnButtonClick(collectible); });
+        if (collectible == null || GameManager.Instance.UnlockedCollectibles.Length == 0) return;
+
+        // Page gauche
+        int indexGauche = m_page * 2;
+        if (indexGauche < collectible.Length)
         {
-            page.text = collectible.Description;
+            NoteDisplay[0].text = collectible[indexGauche].Description;
+            NoteDisplay[0].gameObject.SetActive(true);
+        }
+        else
+        {
+            NoteDisplay[0].text = "";
+            NoteDisplay[0].gameObject.SetActive(false);
+        }
+        //page droite
+        int indexDroite = m_page * 2 + 1;
+        if (indexDroite < collectible.Length)
+        {
+            NoteDisplay[1].text = collectible[indexDroite].Description;
+            NoteDisplay[1].gameObject.SetActive(true);
+        }
+        else
+        {
+            NoteDisplay[1].text = "";
+            NoteDisplay[1].gameObject.SetActive(false);
         }
 
-        m_spawnText.Add(collectible);   
+        //m_spawnText.Add(collectible);   
         // bz ton pere la tchoing page a la con de carnet 
     }
     // pour afficher prochaine page 
+    /*
     public void NextPage()
     {
-        m_page++;
+        int totalPages = Mathf.CeilToInt(GameManager.Instance.UnlockedCollectibles.Length / 2f);
+        if (m_page < totalPages - 1)
+        {
+            m_page++;
+            CreateText();
+            MettreAJourBoutons();
+        }
     }
     // pour afficher Previus page
     public void PreviusPage()
     {
-        m_page--;
+        if (m_page > 0)
+        {
+            m_page--;
+            CreateText();
+            MettreAJourBoutons();
+        }
+    }
+    */
+    void MettreAJourBoutons()
+    {
+        CollectibleObject[] collectibles = GameManager.Instance.UnlockedCollectibles;
+
+        if (collectibles == null || collectibles.Length == 0)
+        {
+            BtnNext.gameObject.SetActive(false);
+            BtnPrevious.gameObject.SetActive(false);
+            return;
+        }
+
+        int totalPages = Mathf.CeilToInt(collectibles.Length / 2f);
+        BtnPrevious.gameObject.SetActive(m_page > 0);
+        BtnNext.gameObject.SetActive(m_page < totalPages - 1);
     }
     // logique de click sur le bouton 
-    public void OnButtonClick(CollectibleObject collectible)
+    public void OnButtonClick(CollectibleObject[] collectible)
     {
         foreach (TMP_Text show in NoteDisplay)
         {
