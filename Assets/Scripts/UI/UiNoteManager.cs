73ced2cc443fb1e8803893d9781e5f20a61de71a
ampheino <38631146+ampheino@users.noreply.github.com>
2026-04-22 11:09:06 +0200

    j'ai tout cassé fais chier

--- Assets/Scripts/UI/UiNoteManager.cs
+++ Assets/Scripts/UI/UiNoteManager.cs
@@ -24,12 +24,14 @@ public class UiNoteManager : MonoBehaviour
 
     private List<CollectibleObject> m_spawnText;
     private int m_page = 0;
-    
-
+    private int m_indexGauche;
+    private int m_indexDroite;
+         
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        m_spawnText = new();
+        m_indexGauche = m_page * 2;
+        m_indexDroite = m_page * 2 + 1;
 
         CreateText();
 
@@ -58,10 +60,10 @@ public class UiNoteManager : MonoBehaviour
         if (collectible == null || GameManager.Instance.UnlockedCollectibles.Length == 0) return;
 
         // Page gauche
-        int indexGauche = m_page * 2;
-        if (indexGauche < collectible.Length)
+        
+        if (m_indexGauche < collectible.Length)
         {
-            NoteDisplay[0].text = collectible[indexGauche].Description;
+            NoteDisplay[0].text = collectible[m_indexGauche].Description;
             NoteDisplay[0].gameObject.SetActive(true);
         }
         else
@@ -70,10 +72,10 @@ public class UiNoteManager : MonoBehaviour
             NoteDisplay[0].gameObject.SetActive(false);
         }
         //page droite
-        int indexDroite = m_page * 2 + 1;
-        if (indexDroite < collectible.Length)
+        
+        if (m_indexDroite < collectible.Length)
         {
-            NoteDisplay[1].text = collectible[indexDroite].Description;
+            NoteDisplay[1].text = collectible[m_indexDroite].Description;
             NoteDisplay[1].gameObject.SetActive(true);
         }
         else
@@ -86,7 +88,7 @@ public class UiNoteManager : MonoBehaviour
         // bz ton pere la tchoing page a la con de carnet 
     }
     // pour afficher prochaine page 
-    /*
+    
     public void NextPage()
     {
         int totalPages = Mathf.CeilToInt(GameManager.Instance.UnlockedCollectibles.Length / 2f);
@@ -97,7 +99,7 @@ public class UiNoteManager : MonoBehaviour
             MettreAJourBoutons();
         }
     }
-    // pour afficher Previus page
+    //pour afficher Previus page
     public void PreviusPage()
     {
         if (m_page > 0)
@@ -107,15 +109,17 @@ public class UiNoteManager : MonoBehaviour
             MettreAJourBoutons();
         }
     }
-    */
-    public void CreateCode()
+    
+    void CreateCode()
     {
         RadioSequenceObject[] sequence = GameManager.Instance.UnlockedSequences;
 
-        int indexGauche = m_page * 2;
-        if (indexGauche < sequence.Length)
+        if (sequence == null || GameManager.Instance.UnlockedSequences.Length == 0) return;
+
+        
+        if (m_indexGauche < sequence.Length)
         {
-            CodeDescriptionDisplay[0].text = sequence[indexGauche].ToString();
+            CodeDescriptionDisplay[0].text = sequence[m_indexGauche].ToString();
             CodeDescriptionDisplay[0].gameObject.SetActive(true);
 
         }
@@ -124,10 +128,10 @@ public class UiNoteManager : MonoBehaviour
             CodeDescriptionDisplay[0].text = "";
             CodeDescriptionDisplay[0].gameObject.SetActive(false);
         }
-        int indexDroit = m_page * 2 + 1;
-        if (indexDroit < sequence.Length)
+        
+        if (m_indexDroite < sequence.Length)
         {
-            CodeSequenceDisplay[0].text = sequence[indexDroit].name;
+            CodeSequenceDisplay[0].text = sequence[m_indexDroite].name;
             CodeSequenceDisplay[0].gameObject.SetActive(true);
         }
         else
