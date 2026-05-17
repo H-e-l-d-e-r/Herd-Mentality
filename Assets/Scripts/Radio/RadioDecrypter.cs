c1d1aac8b75a8ec480d0fdb5551ef6341e92622d
Ernest <luca@marpeau.web4me.fr>
2026-05-17 18:11:59 +0200

    added modifiers and codes to encrypted messages

--- Assets/Scripts/Radio/RadioDecrypter.cs
+++ Assets/Scripts/Radio/RadioDecrypter.cs
@@ -9,18 +9,26 @@ using UnityEngine.UI;
 
 public class RadioDecrypter : MonoBehaviour
 {
+    public UIGroupBehaviour Groups;
+
     [Header("Components")]
     public Button RecordButton;
     public Button TranslateButton;
     public Slider ProgressBar;
+    public UISelectNumber[] NumberSelectors;
 
     [Header("Behaviours")]
     public UISelectDecryptionMode DecryptionModeSelector;
     public DialogueTypewritter Typewritter;
 
+    [Header("Buttons")]
+    public Color DefaultColor;
+    public Color ActionnableColor;
+
     private SequenceObject m_recordObject;
     private bool m_isRecordRunning;
     private bool m_isTranslateRunning;
+    private bool m_isTranslateRunningTwice;
 
     private float m_recordDuration;
     private Coroutine m_recordRoutine;
@@ -32,10 +40,16 @@ public class RadioDecrypter : MonoBehaviour
         NullComponents.ThrowIfNull(DecryptionModeSelector);
         NullComponents.ThrowIfNull(Typewritter);
 
+        if(NumberSelectors.Length == 0)
+        {
+            Debug.LogError("Number Selector is empty.");
+        }
+
         RecordButton.onClick.AddListener(StartRecording);
         TranslateButton.onClick.AddListener(StartTranslating);
         DecryptionModeSelector.OnChange.AddListener((_) => StartTranslating());
 
+        Groups.CurrentGroup = 0; 
     }
 
     void Update()
@@ -63,8 +77,6 @@ public class RadioDecrypter : MonoBehaviour
         m_isRecordRunning = false;
     }
 
-
-
     IEnumerator Record()
     {
         if (!RadioManager.Instance.RadioBehaviour.IsOn)
@@ -89,6 +101,7 @@ public class RadioDecrypter : MonoBehaviour
             Typewritter.TryEnqueueCommand(new DialogueCommand("", null, "Signal not found!"));
 
             m_recordObject = null;
+            m_isRecordRunning = false;
             yield break;
         }
 
@@ -115,25 +128,40 @@ public class RadioDecrypter : MonoBehaviour
         m_isRecordRunning = false;
     }
 
+    /// <summary>
+    /// Translation coroutine task
+    /// This is the main tranlation behaviour
+    /// </summary>
+    /// <returns></returns>
     IEnumerator Translate()
     {
+        // if it's off
         if (!RadioManager.Instance.RadioBehaviour.IsOn)
         {
             yield break;
         }
 
-        if (m_isRecordRunning)
-        {
-            yield break;
-        }
-
+        // if nothing is recorded
         if (m_recordObject == null)
         {
             Typewritter.TryEnqueueCommand(new DialogueCommand("", null, "Recording not found"));
+            m_isTranslateRunning = false;
+            m_isTranslateRunningTwice = false;
+            yield break;
+        }
+
+        // if it's while a something is being recorded
+        if (m_isRecordRunning)
+        {
+            m_isTranslateRunning = false;
+            m_isTranslateRunningTwice = false;
             yield break;
         }
 
         Typewritter.Show();
+
+        m_isTranslateRunningTwice = m_isTranslateRunning;
+        m_isTranslateRunning = true;
 
         // when start recording
         DecryptionModes mode = DecryptionModeSelector.Current;
@@ -144,12 +172,49 @@ public class RadioDecrypter : MonoBehaviour
         {
             DecryptionsResults result = results.First();
             Typewritter.Clear();
-            Typewritter.TryEnqueueCommand(new DialogueCommand("", null, result.Content));
+
+            if (!result.DoesAskModifier)
+            {
+                // when there is nothing to ask
+                Typewritter.TryEnqueueCommand(new DialogueCommand("", null, result.GetContent()));            
+            }
+            else
+            {
+                Typewritter.Clear();
+                Typewritter.TryEnqueueCommand(new DialogueCommand("", null, "Waiting for modifier"));
+
+                // select the second group
+                Groups.CurrentGroup = 1;
+
+                // wait for a second input
+                while (!m_isTranslateRunningTwice)
+                {
+                    yield return new WaitForSeconds(Time.deltaTime);
+                }
+
+                int modifier = (int)NumberSelectors[0].Current;
+                for (int i = 1; i < NumberSelectors.Length; i++)
+                {
+                    modifier += (int)((float)NumberSelectors[i].Current * Mathf.Pow(10, i));
+                }
+
+                Typewritter.Clear();
+                Typewritter.TryEnqueueCommand(new DialogueCommand("", null, result.GetContent(modifier)));            
+
+                Groups.CurrentGroup = 0;
+            }
+
+            m_isTranslateRunning = false;
+            m_isTranslateRunningTwice = false;
+            yield break;
         }
         else
         {
             Typewritter.Clear();
             Typewritter.TryEnqueueCommand(new DialogueCommand("", null, "Translation not found"));
+            
+            m_isTranslateRunning = false;
+            m_isTranslateRunningTwice = false;
             yield break;
         }
     }
---
+++ Assets/Scripts/UI/UISelectNumber.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class UISelectNumber : UISelector<UISelectNumber.SelectableNumbers>
+{
+    public enum SelectableNumbers
+    {
+        Zero = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9
+    }
+}
